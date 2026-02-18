b80f9a839b013a2850553d1a93b96d8e2ba6f162
yuhtanak <yuhtanak@yahoo-corp.jp>
2026-02-18 18:27:36 +0900

    docの修正

--- 09_flask/todo/app.py
+++ 09_flask/todo/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-# ファイルからTODOリストを読み込む関数
 def load_todos():
+    """
+    ファイルからTODOリストを読み込む。
+
+    :return: TODO のリスト
+    """
     try:
         with open("todos.txt", "r") as file:
             todos = [line.strip() for line in file]
@@ -13,14 +17,26 @@ def load_todos():
     return todos
 
 
-# TODOリストをファイルに保存する関数
 def save_todos(todos):
+    """
+    TODOリストをファイルに保存する
+
+    :param todos: 保存する TODO リスト
+    """
     with open("todos.txt", "w") as file:
         file.write("\n".join(todos))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """
+    ルートページ。
+
+    GET:
+        TODO一覧を表示する。
+    POST:
+        フォームから送信されたTODOを追加して、ファイルへ保存後にリダイレクトする。
+    """
     todos = load_todos()
     if request.method == "POST":
         new_todo = request.form.get("todo")
@@ -33,6 +49,11 @@ def index():
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
+    """
+    指定されたIDのTODOを削除する。
+
+    :param todo_id: 削除対象のTODOのインデックス
+    """
     todos = load_todos()
     if 0 <= todo_id < len(todos):
         todos.pop(todo_id)
@@ -41,4 +62,5 @@ def delete(todo_id):
 
 
 if __name__ == "__main__":
+    # アプリケーションをデバッグモードで実行
     app.run(debug=True)
